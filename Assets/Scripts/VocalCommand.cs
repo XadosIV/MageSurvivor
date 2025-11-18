ba84d5f4391108d47a6e8042802b503b2869b7f9
Antoine Vuillet <121035207+Antoine-Vuillet@users.noreply.github.com>
2025-11-18 15:08:02 +0100

    feat: vocal command

---
+++ Assets/Scripts/VocalCommand.cs
@@ -0,0 +1,93 @@
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+using Whisper;
+using Whisper.Utils;
+public class VocalCommand : MonoBehaviour
+{
+    public WhisperManager whisper;
+    public MicrophoneRecord microphoneRecord;
+    public bool streamSegments = true;
+    public bool printLanguage = true;
+
+    [Header("UI")]
+    public Button button;
+    public Text buttonText;
+    public Text outputText;
+    public Text timeText;
+    public Toggle vadToggle;
+
+    private string _buffer;
+
+    private void Awake()
+    {
+        whisper.OnNewSegment += OnNewSegment;
+        whisper.OnProgress += OnProgressHandler;
+
+        microphoneRecord.OnRecordStop += OnRecordStop;
+
+        button.onClick.AddListener(OnButtonPressed);
+
+        vadToggle.isOn = microphoneRecord.vadStop;
+        vadToggle.onValueChanged.AddListener(OnVadChanged);
+    }
+
+    private void OnVadChanged(bool vadStop)
+    {
+        microphoneRecord.vadStop = vadStop;
+    }
+
+    private void OnButtonPressed()
+    {
+        if (!microphoneRecord.IsRecording)
+        {
+            microphoneRecord.StartRecord();
+            buttonText.text = "Stop";
+        }
+        else
+        {
+            microphoneRecord.StopRecord();
+            buttonText.text = "Record";
+        }
+    }
+
+    private async void OnRecordStop(AudioChunk recordedAudio)
+    {
+        buttonText.text = "Record";
+        _buffer = "";
+
+        var sw = new Stopwatch();
+        sw.Start();
+
+        var res = await whisper.GetTextAsync(recordedAudio.Data, recordedAudio.Frequency, recordedAudio.Channels);
+        if (res == null || !outputText)
+            return;
+
+        var time = sw.ElapsedMilliseconds;
+        var rate = recordedAudio.Length / (time * 0.001f);
+        timeText.text = $"Time: {time} ms\nRate: {rate:F1}x";
+
+        var text = res.Result;
+        if (printLanguage)
+            text += $"\n\nLanguage: {res.Language}";
+
+        print(text);
+    }
+
+
+    private void OnProgressHandler(int progress)
+    {
+        if (!timeText)
+            return;
+        timeText.text = $"Progress: {progress}%";
+    }
+
+    private void OnNewSegment(WhisperSegment segment)
+    {
+        if (!streamSegments || !outputText)
+            return;
+
+        _buffer += segment.Text;
+        outputText.text = _buffer + "...";
+    }
+}
